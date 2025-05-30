c92eaa53e9fe30a55be49e21c162a5250c635ae9
Jitesh4565 <jitujadhav28@gmail.com>
2025-05-30 23:35:59 +0530

    Bugsss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // ✅ This allows the build to complete even with ESLint errors
+    ignoreDuringBuilds: true, 
   },
 };
 
